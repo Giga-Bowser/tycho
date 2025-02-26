cc13db8544d6641ea34db7a081004de62763b3f8
Giga Bowser <45986823+Giga-Bowser@users.noreply.github.com>
2025-02-26 08:38:39 -0500

    Use newtype for ExprRef

--- src/parser/pool.rs
+++ src/parser/pool.rs
@@ -5,17 +5,36 @@ pub struct ExprPool<'s> {
     pub vec: Vec<Expr<'s>>,
 }
 
-pub type ExprRef = usize;
-
 impl<'s> ExprPool<'s> {
     pub fn new() -> Self {
         Self { vec: Vec::new() }
     }
 
     pub fn add(&mut self, expr: Expr<'s>) -> ExprRef {
-        let idx = self.vec.len() as ExprRef;
+        let idx = ExprRef::from(self.vec.len());
         self.vec.push(expr);
         idx
+    }
+}
+
+type ExprRefInner = usize;
+
+#[derive(Debug, Clone, Copy)]
+#[repr(transparent)]
+pub struct ExprRef(ExprRefInner);
+
+impl From<ExprRef> for usize {
+    #[inline(always)]
+    #[allow(clippy::unnecessary_cast)]
+    fn from(value: ExprRef) -> Self {
+        value.0 as usize
+    }
+}
+
+impl From<usize> for ExprRef {
+    #[inline(always)]
+    fn from(value: usize) -> Self {
+        ExprRef(value as ExprRefInner)
     }
 }
 
@@ -23,12 +42,12 @@ impl<'s> std::ops::Index<ExprRef> for ExprPool<'s> {
     type Output = Expr<'s>;
 
     fn index(&self, index: ExprRef) -> &Self::Output {
-        &self.vec[index]
+        &self.vec[usize::from(index)]
     }
 }
 
 impl<'s> std::ops::IndexMut<ExprRef> for ExprPool<'s> {
     fn index_mut(&mut self, index: ExprRef) -> &mut Expr<'s> {
-        &mut self.vec[index]
+        &mut self.vec[usize::from(index)]
     }
 }
